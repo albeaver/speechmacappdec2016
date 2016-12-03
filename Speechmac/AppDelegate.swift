4e831de1839619396b64a161c939761d279e6f4a
albeaver <albeaver@alexs-macbook-air.local>
2016-12-02 16:04:35 -0800

    Initial Commit

---
+++ Speechmac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Speechmac
+//
+//  Created by Alexander Beaver on 12/2/16.
+//  Copyright © 2016 Alex Beaver and Bobby Youstra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Speechmac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Speechmac
+//
+//  Created by Alexander Beaver on 12/2/16.
+//  Copyright © 2016 Alex Beaver and Bobby Youstra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
