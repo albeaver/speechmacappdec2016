9d818db1cfedff7e6c44ba9d73f0ba7c9c68bc18
albeaver <albeaver@alexs-macbook-air.local>
2016-12-02 19:04:51 -0800

    Modified UI

--- Speechmac/ViewController.swift
+++ Speechmac/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import Cocoa
-// This is ViewController.swift, where we will put all of the code for the main storyboard
+// This is ViewController.swift, where we will put all of the code for the main storyboard -A
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad() { //This function is used for the FIRST INITIALIZATION of the View Controller. Do not use for something that needs to happen each time a view controller is loaded
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear() { //Use this for things that have to be done each time the view appears
+        
     }
 
     override var representedObject: Any? {
